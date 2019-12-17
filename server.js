e8e8ebcbc353275c09124726226aa47ff72761f9
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-17 15:43:24 -0600

    connect-session-knex session tables

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
+// dependencies!
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
-const sessions = require('express-session');
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
+// Routes for Router & DB config
 const ApiRouter = require("./api/api-router.js");
+const knex = require("./data/db-config");
 
+// start server!!
 const server = express();
 
-const sessionConfiguration = {
-  name: 'secret',
-  secret: 'keep it secret, keep it safe!', 
+const sessionConfig = {
+  name: "secret",
+  secret: "keep it secret, keep it safe!",
   saveUninitialized: false,
   resave: false,
-  store: ,
+  store: new KnexSessionStore({
+    knex,
+    createtable: true,
+    clearInterval: 1000 * 60 * 10, //10 minutes
+    sidfieldname: "sid",
+    tablename: "sessions"
+  }),
   cookie: {
     maxAge: 1000 * 60 * 10, //10 minutes
     secure: false, // true in production
     httpOnly: true //JS can't read this.
   }
-}
+};
 
 server.use(express.json());
 server.use(helmet());
